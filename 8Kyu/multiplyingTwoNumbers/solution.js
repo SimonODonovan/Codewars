6ded86772bc97e4be542ba4bd3ecf86a91c83b98
Simon O'Donovan <stawgzilla@gmail.com>
2022-03-24 21:34:06 +0000

    Added  the following 8kyu solutions:  - Merge two sorted arrays.  - Multiplying two numbers.  - Aspect Ratio Cropping - Part 1

---
+++ 8Kyu/multiplyingTwoNumbers/solution.js
@@ -0,0 +1,10 @@
+/*
+    Function 3 - multiplying two numbers
+    
+    Implement a function which multiplies two numbers.
+ */
+function multiplyTwo(x, y) {
+    return x * y;
+}
+
+module.exports = multiplyTwo;
