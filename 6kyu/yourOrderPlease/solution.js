b3f832696113c9b21d689e27ebdb192610ce4deb
Simon O'Donovan <stawgzilla@gmail.com>
2022-04-19 09:17:53 +0100

    Added 6kyu solutions:  - Your order, please

---
+++ 6kyu/yourOrderPlease/solution.js
@@ -0,0 +1,35 @@
+/*
+    Your order, please
+    
+    Your task is to sort a given string. Each word in the string will 
+    contain a single number. This number is the position the word should 
+    have in the result.
+
+    Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+    If the input string is empty, return an empty string. The words in the 
+    input String will only contain valid consecutive numbers.
+ */
+function order(words) {
+    if(words.length == 0) return "";
+    const splitWords = words.split(" ");
+    const orderedWordsArray = new Array(splitWords.length-1);
+    for(let word of splitWords) {
+        for(let char of word) {
+            if(Number.isInteger(parseInt(char))) {
+                orderedWordsArray[+char-1] = word;
+                break;
+            }
+        }
+    }
+    return orderedWordsArray.join(" ");
+}
+
+function orderAlt(words) {
+    return words
+        .split(" ")
+        .sort((a, b) => a.match(/\d/) - b.match(/\d/))
+        .join(" ");
+}
+
+module.exports = order;
