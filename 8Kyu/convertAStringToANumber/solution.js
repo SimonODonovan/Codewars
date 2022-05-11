979a3078c672dedd4c5ce90ce53ae8f57cbe969b
Simon O'Donovan <stawgzilla@gmail.com>
2022-05-11 15:44:28 +0100

    Added 8kyu solutions:  - Convert a String to a Number!

---
+++ 8Kyu/convertAStringToANumber/solution.js
@@ -0,0 +1,10 @@
+/*
+    Convert a String to a Number!
+    
+    We need a function that can transform a string into a number.
+ */
+var stringToNumber = function (str) {
+    return +str;
+}
+
+module.exports = stringToNumber;
