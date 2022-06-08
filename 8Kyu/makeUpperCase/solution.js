b296398fef483eea87fcee1899c2e725f19fc283
Simon O'Donovan <stawgzilla@gmail.com>
2022-06-08 08:58:55 +0100

    Added 8kyu solutions:  - MakeUpperCase

---
+++ 8Kyu/makeUpperCase/solution.js
@@ -0,0 +1,16 @@
+/*
+    MakeUpperCase
+    
+    Write a function which converts the input string 
+    to uppercase.
+ */
+
+// P.aramters R.eturns E.xamples P.suedo code
+// single string input
+// single string uppercase
+// "art" -> "ART"
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+module.exports = makeUpperCase;
---
+++ 8Kyu/makeUpperCase/solution.py
@@ -0,0 +1,12 @@
+'''
+MakeUpperCase
+    
+Write a function which converts the input string to uppercase.
+'''
+
+# P.aramters R.eturns E.xamples P.suedo code
+# single string input
+# single string uppercase
+# "art" -> "ART"
+def make_upper_case(s):
+    return s.upper()
